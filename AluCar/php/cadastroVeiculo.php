059673af96d41d672f324d51c2a608cff1594346
lucassilva11 <69371948+lucassilva11@users.noreply.github.com>
2022-10-12 16:49:41 -0300

    Inserção imagem no banco.

--- AluCar/php/cadastroVeiculo.php
+++ AluCar/php/cadastroVeiculo.php
@@ -2,6 +2,7 @@
 include_once "conexao.php";
 session_start();
 $id_veiculo = $_SESSION["id_veiculo"];
+$time = time();
 
 $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
@@ -10,6 +11,7 @@ if($id_veiculo > 0){
     "UPDATE veiculo 
         SET  modelo = :modelo
             ,marca = :marca
+            ,imagem = :imagem
             ,ano = :ano
             ,cambio = :cambio
             ,direcao = :direcao
@@ -26,11 +28,15 @@ if($id_veiculo > 0){
             ,id_locadora = :id_locadora
         WHERE id_veiculo = :id_veiculo"; 
 }else{
+    $countfiles = count($_FILES['files']['name']);
+
     $query_veiculo = 
     "INSERT
         INTO veiculo(
              modelo
             ,marca
+            ,img_name
+            ,imagem
             ,ano
             ,cambio
             ,direcao
@@ -49,6 +55,8 @@ if($id_veiculo > 0){
         VALUES (
          :modelo
         ,:marca
+        ,:img_name
+        ,:imagem
         ,:ano
         ,:cambio
         ,:direcao
@@ -67,22 +75,56 @@ if($id_veiculo > 0){
 }
 
 $cad_veiculo = $conn->prepare($query_veiculo);
-$cad_veiculo->bindParam(':modelo', $dados['modelo']);
-$cad_veiculo->bindParam(':marca', $dados['marca']);
-$cad_veiculo->bindParam(':ano', $dados['ano']);
-$cad_veiculo->bindParam(':cambio', $dados['cambio']);
-$cad_veiculo->bindParam(':direcao', $dados['direcao']);
-$cad_veiculo->bindParam(':categoria', $dados['categoria']);
-$cad_veiculo->bindParam(':chassi', $dados['chassi']);
-$cad_veiculo->bindParam(':placa', $dados['placa']);
-$cad_veiculo->bindParam(':cor', $dados['cor']);
-$cad_veiculo->bindParam(':motor', $dados['motor']);
-$cad_veiculo->bindParam(':portas', $dados['portas']);
-$cad_veiculo->bindParam(':qtd_passageiros', $dados['qtd_passageiros']);
-$cad_veiculo->bindParam(':ar_condicionado', $dados['ar_condicionado']);
-$cad_veiculo->bindParam(':valor_hora', $dados['valor_hora']);
-$cad_veiculo->bindParam(':valor_seguro', $dados['valor_seguro']);
-$cad_veiculo->bindParam(':id_locadora', $_SESSION['id_locadora']);
+
+ // Loop all files
+for($i = 0; $i < $countfiles; $i++) {
+  
+    // File name
+    $filename = $_FILES['files']['name'][$i];
+  
+    // Location
+    $target_file = '../image/'.$filename;
+  
+    // file extension
+    $file_extension = pathinfo(
+        $target_file, PATHINFO_EXTENSION);
+         
+    $file_extension = strtolower($file_extension);
+
+    // Valid image extension
+    $valid_extension = array("png","jpeg","jpg");
+  
+    if(in_array($file_extension, $valid_extension)) {
+
+        // Upload file
+        if(move_uploaded_file(
+            $_FILES['files']['tmp_name'][$i],
+            $target_file)){
+                $cad_veiculo->bindParam(':modelo', $dados['modelo']);
+                $cad_veiculo->bindParam(':marca', $dados['marca']);
+                $cad_veiculo->bindParam(':img_name', $filename);
+                $cad_veiculo->bindParam(':imagem', $target_file);
+                $cad_veiculo->bindParam(':ano', $dados['ano']);
+                $cad_veiculo->bindParam(':cambio', $dados['cambio']);
+                $cad_veiculo->bindParam(':direcao', $dados['direcao']);
+                $cad_veiculo->bindParam(':categoria', $dados['categoria']);
+                $cad_veiculo->bindParam(':chassi', $dados['chassi']);
+                $cad_veiculo->bindParam(':placa', $dados['placa']);
+                $cad_veiculo->bindParam(':cor', $dados['cor']);
+                $cad_veiculo->bindParam(':motor', $dados['motor']);
+                $cad_veiculo->bindParam(':portas', $dados['portas']);
+                $cad_veiculo->bindParam(':qtd_passageiros', $dados['qtd_passageiros']);
+                $cad_veiculo->bindParam(':ar_condicionado', $dados['ar_condicionado']);
+                $cad_veiculo->bindParam(':valor_hora', $dados['valor_hora']);
+                $cad_veiculo->bindParam(':valor_seguro', $dados['valor_seguro']);
+                $cad_veiculo->bindParam(':id_locadora', $_SESSION['id_locadora']);
+            }
+        }
+}
+
+
+
+
 
 if($id_veiculo > 0){
     $cad_veiculo->bindParam(':id_veiculo', $id_veiculo);
