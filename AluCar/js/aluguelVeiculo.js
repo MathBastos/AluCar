d312041a5734f7f424353e420d6db2a94dd9d714
Matheus Bastos <matheus.hebastos@gmail.com>
2022-11-10 20:41:23 -0300

    Ajuste front aluguel
    Ajuste em elementos front-end da tela de aluguel de veiculos

--- AluCar/js/aluguelVeiculo.js
+++ AluCar/js/aluguelVeiculo.js
@@ -6,10 +6,18 @@ function buscaVeiculo(){
         url: "../php/buscaVeiculo.php",
         success: function (resultado) {
             document.getElementById("modelo").value = resultado.modelo;
+            document.getElementById("marca").value = resultado.marca;
+            document.getElementById("ano").value = resultado.ano;
+            document.getElementById("cambio").value = resultado.cambio;
+            document.getElementById("direcao").value = resultado.direcao;
+            document.getElementById("categoria").value = resultado.categoria;
+            document.getElementById("cor").value = resultado.cor;
+            document.getElementById("motor").value = resultado.motor;
+            document.getElementById("portas").value = resultado.portas;
+            document.getElementById("qtd_passageiros").value = resultado.qtd_passageiros;
+            document.getElementById("ar_condicionado").value = (resultado.ar_condicionado == 's' ? "Sim" : "Não" );
 
-            console.log(resultado.imagem)
-
-            var html = "<img scr=" + resultado.imagem + " width='100px' height='100px'/>";
+            var html = "<img src=" + resultado.imagem + " width='100%' height='50%'/>";
             $("#imagem").html(html);
 
         },
